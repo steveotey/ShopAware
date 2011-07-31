19ee372f2a4594e9a160dc03814c00ca37cd469a
Steve Otey <odiwan@Steve-Oteys-MacBook-Pro.local>
2011-07-31 19:57:23 -0400

    Initial Commit

---
+++ ShopAware/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  ShopAware
+//
+//  Created by Steve Otey on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ ShopAware/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  ShopAware
+//
+//  Created by Steve Otey on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
